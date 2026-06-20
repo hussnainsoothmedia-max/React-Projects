19c133926e95789316605178f520bdeb7cef1ac2
hussnainsoothmedia-max <hussnain.soothmedi@gmail.com>
2026-06-20 20:57:21 +0500

    Update data file with latest entries

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,8 @@ function App() {
   const savetoLS = (params) => {
     localStorage.setItem("todos", JSON.stringify(params));
   };
-  const togglefinish = (params) => {
+  const togglefinish = (e) => {
+    setfinish(!finish)
     
   }
   
@@ -68,18 +69,19 @@ function App() {
     <>
       <Navbar />
 
-      <div className="container bg-slate-200 my-5 rounded-xl p-5 min-h-[80vh] w-[90vw] mx-auto ">
-        <div className="addTodos my-5">
+      <div className="container bg-slate-200 my-5 rounded-xl p-5 min-h-[80vh] w-1/2 mx-auto ">
+        <div className="addTodos my-5 flex flex-col gap-5">
+          <h1 className="font-bold text-center text-xl">iTask - Manage your task in one place</h1>
           <h1 className="text-lg font-bold">Add Your todo</h1>
           <input
             onChange={handleChange}
             value={todo}
             type="text"
-            className="w-1/2"
+            className="w-full"
           />
           <button
             onClick={handleAdd} disabled ={todo.length<3}
-            className="bg-slate-800 disabled:bg-slate-700 hover:bg-slate-950 p-3 py-1 text-sm  font-bold text-white rounded-md mx-6"
+            className="bg-slate-800 disabled:bg-slate-700 hover:bg-slate-950 p-3 py-1 text-sm  font-bold text-white rounded-md"
           >
             Add
           </button>
@@ -89,10 +91,10 @@ function App() {
         <div className="todos">
           {todos.length === 0 && <div>No Todo Display</div>}
           {todos.map((item) => {
-            return (
+            return (finish || !item.isComplete) && (
               <div
                 key={item.id}
-                className="todo flex justify-between w-1/4 my-3"
+                className="todo flex justify-between w-full my-3"
               >
                 <div className="flex gap-5">
                   <input
